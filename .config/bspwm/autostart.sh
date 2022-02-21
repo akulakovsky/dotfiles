20a8849762e5405b6288e66fd532852631e2a5c7
akulakovsky <akulakovsky@gmail.com>
2022-02-20 16:56:34 -0800

    Yeah

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -13,14 +13,5 @@ xmousepasteblock &
 $HOME/.config/polybar/launch.sh &
 run sxhkd &
 xsetroot -cursor_name left_ptr &
-#conky -c $HOME/.config/bspwm/system-overview &
-#run nm-applet &
-#run pamac-tray &
-#run xfce4-power-manager &
-#numlockx on &
-#blueberry-tray &
-picom --config $HOME/.config/bspwm/picom.conf &
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-#/usr/lib/xfce4/notifyd/xfce4-notifyd &
-#run volumeicon &
+picom &
 nitrogen --restore &
